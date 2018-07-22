fdc9ccea9f59014c4d97c13c24716850be86c3ee
Quentin Leroy DS4 <leroy.quentin456@gmail.com>
2018-07-23 00:36:59 +0200

    repocompare improvment
    TreeSimulation and repocompare

--- RepoCompare.ps1
+++ RepoCompare.ps1
@@ -1,15 +1,18 @@
 ﻿
 #Repository comparison
 
-#Takes two repository path in input, tell if they contains the exact same content (subfolder tree and files, based on names)
+#Takes two repository path in input, tell if they contain the exact same content (subfolder tree and files, based on names)
 #And further, based on names, last modification date and size.
 
+Write-Host ""
 Write-Host "            ##############################"
 Write-host "            # Repository comparison tool #"
 Write-Host "            ##############################"
+Write-Host ""
 
 #Test de comparaison de deux arborescences
 $Choice = Read-Host -Prompt 'Comparer Dossiers uniquement "1", fichiers uniquement "2", dossiers et fichiers "3"'
+[bool]$ChoiceCorrect = 1
 
 #Folder only
 if($Choice -eq "1"){
@@ -19,21 +22,6 @@ if($Choice -eq "1"){
     #Récupération des éléments de l'arbre
     $Tree1 = Get-ChildItem -Path $TreePath1 -Name -Recurse -Directory
     $Tree2 = Get-ChildItem -Path $TreePath2 -Name -Recurse -Directory
-
-    [bool]$isSame = 1
-    For ($i = 0 ; $i –lt $Tree1.length ; $i ++){
-    
-        if($Tree1[$i] -ne $Tree2[$i]){
-            write-host "Error at index"$i
-            write-host "Tree1 has"$Tree1[$i] 
-            write-host "Tree2 has"$Tree2[$i]
-            $isSame = 0
-        }
-    }
-
-    if($isSame -eq 1){
-        write-host "Les deux arbres sont identiques"
-    }
 }
 #Files only
 elseif($Choice -eq "2"){
@@ -43,21 +31,6 @@ elseif($Choice -eq "2"){
     #Récupération des éléments de l'arbre
     $Tree1 = Get-ChildItem -Path $TreePath1 -Name -Recurse -File
     $Tree2 = Get-ChildItem -Path $TreePath2 -Name -Recurse -File
-
-    [bool]$isSame = 1
-    For ($i = 0 ; $i –lt $Tree1.length ; $i ++){
-    
-        if($Tree1[$i] -ne $Tree2[$i]){
-            write-host "Error at index"$i
-            write-host "Tree1 has"$Tree1[$i] 
-            write-host "Tree2 has"$Tree2[$i]
-            $isSame = 0
-        }
-    }
-
-    if($isSame -eq 1){
-        write-host "Les deux arbres sont identiques"
-    }
 }
 #Both folders and files
 elseif($Choice -eq "3"){
@@ -67,22 +40,34 @@ elseif($Choice -eq "3"){
     #Récupération des éléments de l'arbre
     $Tree1 = Get-ChildItem -Path $TreePath1 -Name -Recurse
     $Tree2 = Get-ChildItem -Path $TreePath2 -Name -Recurse
+}
+else{
+    write-host "Incorrect choice"
+    $ChoiceCorrect = 0
+}
 
+if($ChoiceCorrect -eq 1){
     [bool]$isSame = 1
-    For ($i = 0 ; $i –lt $Tree1.length ; $i ++){
+    if($Tree1.Length -eq $Tree2.Length){
+        #Compare Tree1 and Tree2
+        For ($i = 0 ; $i –lt $Tree1.length ; $i ++){
     
-        if($Tree1[$i] -ne $Tree2[$i]){
-            write-host "Error at index"$i
-            write-host "Tree1 has"$Tree1[$i] 
-            write-host "Tree2 has"$Tree2[$i]
-            $isSame = 0
+            if($Tree1[$i] -ne $Tree2[$i]){
+                write-host "Error at index"$i
+                write-host "Tree1 has"$Tree1[$i] 
+                write-host "Tree2 has"$Tree2[$i]
+                $isSame = 0
+            }
         }
+    }
+    else{ 
+        $isSame = 0
+        write-host "Les deux arbres ne sont pas de meme longueur"
     }
 
     if($isSame -eq 1){
         write-host "Les deux arbres sont identiques"
     }
 }
-else{
-    write-host "Incorrect choice"
-}
+
+
---
+++ TreeSimulation.ps1
@@ -0,0 +1,9 @@
+﻿# Tree creationfor test purpose
+
+set-location "C:\Users\Quentin\Dev\PowerShell_BackUp\Test\"
+
+New-Item .\d1 -type directory
+New-Item .\d1\d11 -type directory 
+New-Item .\d1\d12 -type directory 
+New-Item .\d2\d21 -type directory
+New-Item .\d2\d22 -type directory 
