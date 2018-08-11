3cce03541a7bd29b97c8e1d6f11335df955db8f1
quentin leroy <quentin.leroy@stcorp.nl>
2018-08-12 00:53:10 +0200

    repo compare with last write time and size

--- RepoCompare.ps1
+++ RepoCompare.ps1
@@ -28,7 +28,7 @@ if($Choice -eq "1"){
         For ($i = 0 ; $i –lt $Tree1.length ; $i ++){
     
             if($Tree1[$i] -ne $Tree2[$i]){
-                write-host "Error at index"$i
+                write-host "Difference at index"$i
                 write-host "Tree1 has"$Tree1[$i] 
                 write-host "Tree2 has"$Tree2[$i]
                 $isSame = 0
@@ -53,19 +53,53 @@ elseif($Choice -eq "2"){
     $Tree1 = Get-ChildItem -Path $TreePath1 -Name -Recurse -File
     $Tree2 = Get-ChildItem -Path $TreePath2 -Name -Recurse -File
 
+    #(Get-ItemProperty .\DFS.ps1).LastWriteTime
+
     [bool]$isSame = 1
+    [bool]$isSameSize = 1
+    [bool]$isSameLWT = 1
+    $diff_counter = 0
     if($Tree1.Length -eq $Tree2.Length){
         #Compare Tree1 and Tree2
         For ($i = 0 ; $i –lt $Tree1.length ; $i ++){
-    
-            if($Tree1[$i] -ne $Tree2[$i]){
-                write-host "Error at index"$i
+            #Name comparison: if the names are the same, size and last write time can be compared
+            if($Tree1[$i] -eq $Tree2[$i]){
+
+                #Lengh (Size in Bytes)
+                $Size1 = (Get-ItemProperty $Tree1[$i]).Length
+                $Size1 = (Get-ItemProperty $Tree2[$i]).Length
+                if($Size1 -ne $Size2){
+                    write-host "File size is different at index"$i
+                    write-host "Tree1 : "$Tree1[$i]" Size = "$LWT1 "Bytes"
+                    write-host "Tree2 : "$Tree2[$i]" Size = "$LWT2 "Bytes"
+                    $isSame = 0
+                    $isSameSize = 0
+                }
+                #Last Write Time
+                $LWT1 = (Get-ItemProperty $Tree1[$i]).LastWriteTime
+                $LWT2 = (Get-ItemProperty $Tree2[$i]).LastWriteTime
+                if($LWT1.ToString() -ne $LWT2.ToString()){
+                    write-host "Last Write Time is different at index"$i
+                    write-host "Tree1 : "$Tree1[$i]" Last Write Time = "$LWT1 
+                    write-host "Tree2 : "$Tree2[$i]" Last Write Time = "$LWT2
+                    $isSame = 0
+                    $isSameLWT = 0
+                }
+                if(($isSameSize -eq 0) -or ($isSameLWT -eq 0)){
+                    $isSameSize = 1
+                    $isSameLWT = 1
+                    $diff_counter++
+                }
+            }#If File Name
+            else{ #File names are different
+                write-host "Name difference at index"$i
                 write-host "Tree1 has"$Tree1[$i] 
                 write-host "Tree2 has"$Tree2[$i]
                 $isSame = 0
+                $diff_counter++
             }
-        }
-    }
+        }#For Loop
+    }#If Lengh 
     else{ 
         $isSame = 0
         write-host "Les deux arbres ne sont pas de meme longueur"
@@ -73,6 +107,10 @@ elseif($Choice -eq "2"){
 
     if($isSame -eq 1){
         write-host "Les deux arbres sont identiques"
+    }
+    else{
+        Write-Host "Arbres non-identiques."
+        Write-Host "Nombre de difference:"$diff_counter
     }
 }
 #Both folders and files
@@ -90,7 +128,7 @@ elseif($Choice -eq "3"){
         For ($i = 0 ; $i –lt $Tree1.length ; $i ++){
     
             if($Tree1[$i] -ne $Tree2[$i]){
-                write-host "Error at index"$i
+                write-host "Difference at index"$i
                 write-host "Tree1 has"$Tree1[$i] 
                 write-host "Tree2 has"$Tree2[$i]
                 $isSame = 0
--- TreeSimulation.ps1
+++ TreeSimulation.ps1
@@ -1,9 +1,25 @@
 ﻿# Tree creationfor test purpose
 
-set-location "C:\Users\Quentin\Dev\PowerShell_BackUp\Test\"
+#set-location "C:\Users\Quentin\Dev\PowerShell_BackUp\Test\"
 
 New-Item .\d1 -type directory
+New-Item .\d1\f1.txt -type File
+
 New-Item .\d1\d11 -type directory 
+New-Item .\d1\d11\f11.txt -type File
+
 New-Item .\d1\d12 -type directory 
+New-Item .\d1\d12\f12.txt -type File
+
+New-Item .\d2 -type directory
+New-Item .\d2\f2.txt -type File
+
 New-Item .\d2\d21 -type directory
+New-Item .\d2\d21\f21.txt -type File
+
 New-Item .\d2\d22 -type directory 
+New-Item .\d2\d22\f22.txt -type File
+
+#reset:
+#rm d1 -Recurse
+#rm d2 -Recurse
